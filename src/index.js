8ff0c2524b7b5c5e6e5afe913e38ea95e3554318
Andrew Imm <andrewi@fb.com>
2016-09-26 12:13:05 -0700

    Update to Babel 6

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import path from 'path';
 
-export default function({ Plugin, types: t }) {
-  return new Plugin('inline-package-json', {
+export default function({types: t}) {
+  return {
     visitor: {
-      MemberExpression: function MemberExpression(node, parent) {
+      MemberExpression: function MemberExpression(treePath) {
+        const node = treePath.node;
         if (t.isCallExpression(node.object) &&
             t.isIdentifier(node.object.callee, { name: 'require' }) &&
             t.isLiteral(node.object.arguments[0]) &&
             node.object.arguments[0].value.match(/package\.json$/)) {
-          let srcPath = path.resolve(this.state.opts.filename);
+          let srcPath = path.resolve(this.file.opts.filename);
           let pkg = require(path.join(srcPath, '..', node.object.arguments[0].value));
           let value = pkg[node.property.name];
-          return t.expressionStatement(t.valueToNode(value));
+          treePath.replaceWith(t.expressionStatement(t.valueToNode(value)));
         }
       }
     }
-  });
+  }
 }
